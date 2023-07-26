81ad18aad159c6c12802023de4272714f7b358c0
Francis Nathanael De Villena <fnodevillena@gmail.com>
2023-07-27 01:15:35 +0800

    Added prototype UI

--- kalahi_cidss/home/views.py
+++ kalahi_cidss/home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home/home.html")
+    return render(request, "home/home.html", {'active_page': home})
--- kalahi_cidss/kalahi_cidss/urls.py
+++ kalahi_cidss/kalahi_cidss/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
-from home.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-]
+    path('', include('home.urls', namespace='home')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
