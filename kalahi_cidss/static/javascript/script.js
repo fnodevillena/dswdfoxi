e97b77f833a949c506a5f064e0bad222a99c2646
Francis Nathanael De Villena <fnodevillena@gmail.com>
2023-07-28 18:05:18 +0800

    UI updates for Mobile

--- kalahi_cidss/static/javascript/script.js
+++ kalahi_cidss/static/javascript/script.js
@@ -34,6 +34,91 @@ function slideUpNavbarOnScroll() {
 }
 document.addEventListener("DOMContentLoaded", slideUpNavbarOnScroll);
 
+function adjustNavbarHeight() {
+    const navbar = document.querySelector("nav");
+    const sidebarPadding = document.querySelector("aside .sidebar-divider");
+    const orientation = window.matchMedia('(orientation: portrait)').matches ? 'portrait' : 'landscape';
+
+    navbar.style.height = isMobileDevice()
+        ? (orientation === 'portrait' ? '95px' : '60px')
+        : '50px';
+
+    sidebarPadding.style.marginTop = isMobileDevice()
+        ? (orientation === 'portrait' ? '105px' : '70px')
+        : '60px';
+}
+document.addEventListener("DOMContentLoaded", adjustNavbarHeight);
+window.addEventListener('resize', adjustNavbarHeight);
+
+function adjustNavbarLogo() {
+    const navbarLogo = document.querySelector("nav img");
+    const sidebar = document.querySelector("aside");
+    const sidebarPadding = document.querySelector("aside .sidebar-divider");
+    const orientation = window.matchMedia('(orientation: portrait)').matches ? 'portrait' : 'landscape';
+
+    navbarLogo.style.width = isMobileDevice()
+        ? (orientation === 'portrait' ? '250px' : '200px')
+        : '150px';
+
+    sidebar.style.width = isMobileDevice()
+        ? (orientation === 'portrait' ? '250px' : '200px')
+        : '150px';
+
+    sidebarPadding.style.width = isMobileDevice()
+        ? (orientation === 'portrait' ? '270px' : '220px')
+        : '170px';
+}
+document.addEventListener("DOMContentLoaded", adjustNavbarLogo);
+window.addEventListener('resize', adjustNavbarLogo);
+
+function toggleNavbarList() {
+    const navbarDivider = document.querySelector("nav .navbar-divider");
+    const navbarList = document.querySelector("nav ul");
+
+    navbarDivider.style.display = isMobileDevice() ? 'none' : 'block';
+    navbarList.style.display = isMobileDevice() ? 'none' : 'block';
+}
+document.addEventListener("DOMContentLoaded", toggleNavbarList);
+window.addEventListener('resize', toggleNavbarList);
+
+function toggleSidebar() {
+    const sidebar = document.querySelector('aside');
+    const overlay = document.querySelector('.overlay');
+    const isActive = sidebar.classList.toggle('active');
+  
+    overlay.style.display = isActive ? 'block' : 'none';
+  }
+  
+  function toggleNavbarLogo() {
+    const logo = document.querySelector("nav img");
+    const desktopURL = "https://kalahi.dswd.gov.ph/";
+  
+    logo.addEventListener("click", function () {
+      if (isMobileDevice()) {
+        toggleSidebar();
+      } else {
+        window.location.href = desktopURL;
+      }
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", toggleNavbarLogo);
+
+
+function hideSidebar() {
+    const sidebar = document.querySelector("aside");
+    const sidebarDivider = document.querySelector("aside .sidebar-divider");
+    const sidebarList = document.querySelector("aside ul");
+
+    sidebar.style.display = isMobileDevice() ? 'block' : 'none';
+    sidebarDivider.style.display = isMobileDevice() ? 'block' : 'none';
+    sidebarList.style.display = isMobileDevice() ? 'block' : 'none';
+}
+document.addEventListener("DOMContentLoaded", hideSidebar);
+window.addEventListener('resize', hideSidebar);
+
+  
+
 
 
 /* 
